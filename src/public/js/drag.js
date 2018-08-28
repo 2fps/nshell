e20499b6e747e24853ae58659718fa84480d2b6d
2fps <echoweb@126.com>
2018-08-28 14:08:15 +0800

    add move

--- src/public/js/drag.js
+++ src/public/js/drag.js
@@ -6,26 +6,29 @@
 let dragging = false,   // 表示是否在拖拽中
     mouseX = 0,         // 表示鼠标x轴位置
     mouseY = 0,         // 表示鼠标y轴位置
-    ipcRenderer = window.electron.ipcRenderer;
-// let aa = require('electron');
-    ipcRenderer.send('somemsg', 'aaaa');
+    mainWindow = window.electron.remote.getCurrentWindow();
 
 window.addEventListener('mousedown', function(e) {
-    dragging = true;
+    // 提前获得位置
     mouseX = e.pageX;
     mouseY = e.pageY;
-
-    console.log(mouseX, mouseY);
+    window.addEventListener('mousemove', mousemoveFn);
 });
 
-// window.addEventListener('mouseup', function(e) {
-//     dragging = false;
-// });
+window.addEventListener('mouseup', function(e) {
+    window.removeEventListener('mousemove', mousemoveFn);
+});
 
-// window.addEventListener('mousemove', function(e) {
-//     if (dragging) {
-//         let pos = mainWindow.getPosition();
+function mousemoveFn(e) {
+    let moveX = e.pageX - mouseX,
+        moveY = e.pageY - mouseY,
+        pos = mainWindow.getPosition();     // 当前窗口位置
+    
+    // 重新计算
+    pos[0] = pos[0] + moveX;
+    pos[1] = pos[1] + moveY;
 
-//         pos[0] = pos[0] + mouseX
-//     }
-// });
+    mainWindow.setPosition(pos[0], pos[1], true);   // 重新设置窗口的位置
+    mouseX = e.pageX;
+    mouseY = e.pageY;
+}
